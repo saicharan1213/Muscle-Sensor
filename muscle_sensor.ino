bf1a5dd033972756f67d1756d62b1614af384131
saicharan1213 <67377032+saicharan1213@users.noreply.github.com>
2022-01-03 18:44:38 +0530

    Add files via upload

---
+++ muscle_sensor.ino
@@ -0,0 +1,53 @@
+
+
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+const char* ssid = "Saketh";
+const char* pass = "123456789";
+
+const char* mqttServer = "broker.hivemq.com";
+const int mqttPort = 1883;
+
+const int threshold = 1000;
+int led = 2;           // the PWM pin the LED is attached to
+
+WiFiClient espwifi;
+PubSubClient client(espwifi);
+  
+void setup() 
+{
+  pinMode(led, OUTPUT);
+  Serial.begin(115200); 
+  WiFi.begin(ssid,pass);
+  while(WiFi.status()!=WL_CONNECTED)
+  {
+    Serial.print('.');
+    delay(500);
+   }
+  Serial.println("wifi connected");
+   client.setServer(mqttServer,mqttPort);
+   while(!client.connected())
+   {
+   if(client.connect("muscleSensor"))
+    {
+      Serial.println("mqtt established");
+    }
+   }
+}
+
+void loop() 
+{
+ int y1 = analogRead(A0);
+ if(y1>threshold)
+  {
+  digitalWrite(led,LOW);
+  client.publish("muscle/vjit/mini","Person in danger");
+  
+  Serial.println("data sent");
+  }
+ digitalWrite(led,HIGH);
+ Serial.println(y1);
+ client.loop();
+ delay(1500);
+}
